051aa29c9223a82dd885e628523583a75b05fa73
M_ALi <alihassanr636@gmail.com>
2025-07-21 23:48:45 +0500

    button color change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -289,7 +289,7 @@ const Navbar = () => {
             link="/signup"
             type="button"
             children={"Create an account"}
-            className="text-lg font-semibold px-4 py-2.5 rounded-full bg-white text-black hover:text-white hover:bg-[#3056D3]"
+            className="text-lg font-semibold px-4 py-2.5 rounded-full bg-[#3056D3] text-black hover:text-white hover:bg-zinc-900"
           />
         </div>
       </div>
